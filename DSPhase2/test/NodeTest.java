8b81eabe83e2072259690119d2793614457f9c70
keshani <keshani.rathanayaka@gmail.com>
2015-04-19 11:56:21 +0530

    added test

---
+++ DSPhase2/test/NodeTest.java
@@ -0,0 +1,42 @@
+/*
+ * CS4262 Distributed Systems Mini Project
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Keshani
+ */
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
